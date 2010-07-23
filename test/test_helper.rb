830c5d3bde00f6dcb2f8c085c4074c4a0928eeba
Rico Sta. Cruz <rico@ambiescent.com>
2010-07-24 05:48:41 +0800

    Add test helpers.

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'init.rb')
+
+require 'rack/test'
+
+class Test::Unit::TestCase
+  include Rack::Test::Methods
+end # class
