a2adeec487be2f049a2b15497b1d856b62407d92
Rico Sta. Cruz <rico@ambiescent.com>
2010-07-25 01:42:59 +0800

    Update config.rb.

--- lib/nano/config.rb
+++ lib/nano/config.rb
@@ -1,32 +1,27 @@
 require 'yaml'
 
-class Main
-  helpers do
-    # Returns a value for a configuration key.
-    #
-    # Params:
-    #   domain: The file.
-    #   key:    The key. Leave blank to return the whole thing.
-    #
-    # Example:
-    #   # Reads datamapper.yml for [:development]['adapter']
-    #   config(:datamapper, 'adapter')  #=> 'mysql'
-    #
-    def config(domain, key=nil)
-      domain = domain.to_sym
+# Returns a value for a configuration key.
+#
+# Params:
+#   domain: The file.
+#   key:    The key. Leave blank to return the whole thing.
+#
+# Example:
+#   # Reads datamapper.yml for [:development]['adapter']
+#   config(:datamapper, 'adapter')  #=> 'mysql'
+#
+def config(domain, key=nil)
+  domain = domain.to_sym
 
-      unless $monk_config
-        $monk_config = Hash.new { Hash.new }
-      end
-
-      unless $monk_config.include?(domain)
-        fname = root_path('config', "#{domain}.yml")
-        $monk_config[domain] = YAML.load_file(fname)  if File.exists?(fname)
-      end
-
-      return $monk_config[domain][RACK_ENV.to_sym] if key.nil?
-      $monk_config[domain][RACK_ENV.to_sym][key]
-    end
+  unless $monk_config
+    $monk_config = Hash.new { Hash.new }
   end
-end
 
+  unless $monk_config.include?(domain)
+    fname = root_path('config', "#{domain}.yml")
+    $monk_config[domain] = YAML.load_file(fname)  if File.exists?(fname)
+  end
+
+  return $monk_config[domain][RACK_ENV.to_sym] if key.nil?
+  $monk_config[domain][RACK_ENV.to_sym][key]
+end
