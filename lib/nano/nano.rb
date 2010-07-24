ccd3330ceebf894f02fe6b2463737316b9ad0e99
Rico Sta. Cruz <rico@ambiescent.com>
2010-07-24 15:11:42 +0800

    Add comments on the config file.

--- lib/nano/nano.rb
+++ lib/nano/nano.rb
@@ -258,10 +258,10 @@ module Nano
       fname = File.join(root_path, 'config', 'nano_sources.list')
       File.open(fname) do |f|
         f.read.split("\n").inject([]) do |a, line|
-          next  if line.match(/^\s*#/)
-          next  if line.count(' ') == 0
-          parts = line.partition(" ")
-          a << { :name => parts[0], :url => parts[2] }
+          unless line.match(/^\s*#/) or line.count(' ') == 0
+            parts = line.partition(" ")
+            a << { :name => parts[0], :url => parts[2] }
+          end
           a
         end
       end
