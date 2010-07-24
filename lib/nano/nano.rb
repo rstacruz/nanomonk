4b17827cbdabdda7d871f30b5949f9dc89d2ff31
Rico Sta. Cruz <rico@ambiescent.com>
2010-07-24 17:11:14 +0800

    Change the declaration of the errors to the better Class.new style.

--- lib/nano/nano.rb
+++ lib/nano/nano.rb
@@ -1,6 +1,6 @@
 module Nano
-  class AlreadyInstalledError < StandardError; end
-  class NoGemError < StandardError; end
+  AlreadyInstalledError = Class.new(StandardError)
+  NoGemError            = Class.new(StandardError)
 
   autoload :Actions, File.join(File.dirname(__FILE__), 'nano', 'actions')
 end
