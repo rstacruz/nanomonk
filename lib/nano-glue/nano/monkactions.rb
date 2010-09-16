5bebf664a8fc863d60b2d5b7c0462801d89db0e8
Rico Sta. Cruz <rico@ambiescent.com>
2010-09-16 17:31:11 +0800

    Compatibility fixes.

--- lib/nano-glue/nano/monkactions.rb
+++ lib/nano-glue/nano/monkactions.rb
@@ -1,17 +1,39 @@
 module Nano::MonkActions
-private
-  def root_path(*args)
-    return @@root_path  if defined?(@@root_path)
-    path = File.dirname(__FILE__)
-    old_path = nil
+  def self.included(klass)
+    klass.extend ClassMethods
+  end
 
-    while path != old_path
-      return (@@root_path = path)  if File.exists?(File.join(path, 'init.rb'))
-
-      old_path = path
-      path = File.expand_path(File.join(path, '..'))
+  module ClassMethods
+    def add_config_file(fname)
+      @config_files ||= Array.new
+      @config_files << "#{root_path}/#{fname}"
     end
-    @@root_path = ''
+
+    def config_files
+      defined?(@config_files) ? @config_files : []
+    end
+
+    def root_path(*args)
+      return @@root_path  if defined?(@@root_path)
+      path = File.dirname(__FILE__)
+      old_path = nil
+
+      while path != old_path
+        if File.exists?(File.join(path, 'init.rb'))
+          return (@@root_path = path)
+        end
+
+        old_path = path
+        path = File.expand_path(File.join(path, '..'))
+      end
+      @@root_path = ''
+    end
+  end
+
+private
+
+  def root_path(*a)
+    self.class.root_path(*a)
   end
 
   def target_file_for(example_file)
@@ -19,10 +41,15 @@ private
   end
 
   def verify_config(env)
-    @@config_files.each { |f| verify(f.gsub('%{env}', env)) }  if defined?(@@config_files)
+    self.class.config_files.each { |f| verify(f.gsub('%{env}', env)) }
   end
 
   def verify(example)
     copy_example(example) unless File.exists?(target_file_for(example))
+  end
+
+  def copy_example(example, target = target_file_for(example))
+    return  if File.exists?(target)
+    File.exists?(example) ? copy_file(example, target) : say_status(:creating, target)
   end
 end
--- lib/thors/monk.thor
+++ lib/thors/monk.thor
@@ -12,21 +12,6 @@ class Monk < Thor
     exec cmd
   end
 
-  desc "copy_example EXAMPLE, TARGET", "Copies an example file to its destination"
-  def copy_example(example, target = target_file_for(example))
-    return  if File.exists?(target)
-    File.exists?(example) ? copy_file(example, target) : say_status(:missing, example)
-  end
-
 private
-  def self.root_path
-    File.expand_path(File.dirname(__FILE__), '../..')
-  end
-
-  def self.add_config_file(fname)
-    @@config_files ||= Array.new
-    @@config_files << "#{root_path}/#{fname}"
-  end
-
   add_config_file 'config/appconfig.example.yml'
 end
